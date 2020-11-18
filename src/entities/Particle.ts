9f46757258adbb1e7f6be4a40d5d42e471537a4e
G3F4 <kamil.grabek@gmail.com>
2020-11-18 02:41:20 +0100

    Entities folder, text prinitng util

---
+++ src/entities/Particle.ts
@@ -0,0 +1,67 @@
+import { randomIntFromRange } from '../utils/array';
+
+export const particlePhases = ['grow', 'shrink'] as const;
+export const particleMaxRadius = 5;
+export const particleMoveRandomness = 3;
+export const particleRadiusGrowSpeed = 0.3;
+
+export default class Particle {
+  constructor(
+    private x: number,
+    private y: number,
+    private radius: number,
+    private color: string | CanvasGradient | CanvasPattern,
+    private phase: 'grow' | 'shrink',
+  ) {}
+
+  draw(ctx: CanvasRenderingContext2D): void {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  update(ctx: CanvasRenderingContext2D): void {
+    this.lifeCycle();
+    this.randomMove();
+    this.draw(ctx);
+  }
+
+  lifeCycle(): void {
+    if (this.phase === 'grow') {
+      this.radius +=
+        particleRadiusGrowSpeed * randomIntFromRange(0, particleMoveRandomness);
+
+      if (this.radius > particleMaxRadius) {
+        this.phase = 'shrink';
+      }
+    }
+
+    if (this.phase === 'shrink') {
+      this.radius -= particleRadiusGrowSpeed;
+
+      if (this.radius < 0) {
+        this.radius = 0;
+        this.phase = 'grow';
+        this.randomPosition();
+      }
+    }
+  }
+
+  randomMove(): void {
+    this.x += randomIntFromRange(
+      -particleMoveRandomness,
+      particleMoveRandomness,
+    );
+    this.y += randomIntFromRange(
+      -particleMoveRandomness,
+      particleMoveRandomness,
+    );
+  }
+
+  randomPosition(): void {
+    this.x = randomIntFromRange(0, innerWidth);
+    this.y = randomIntFromRange(0, innerHeight);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,20 @@
 import { randomIntFromRange } from './utils/array';
 import appLoop from './utils/appLoop';
 import { randomRgba } from './utils/color';
+import Particle, {
+  particleMaxRadius,
+  particlePhases,
+} from './entities/Particle';
+import {
+  clearDrawingArea,
+  getDrawingContext,
+  getMousePosition,
+  registerClickHandler,
+} from './utils/appWindow';
+import getTextPrinter from './utils/getTextPrinter';
 
-const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-const c = canvas.getContext('2d') as CanvasRenderingContext2D;
-
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2,
-};
-
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
-
-addEventListener('click', () => {
-  randomizeParticles();
-});
-
-addEventListener('resize', () => {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-});
-
-const particleMaxRadius = 5;
 const particleCount = (innerWidth * innerHeight) / 100;
-const particleRadiusGrowSpeed = 0.3;
-const particlePhases = ['grow', 'shrink'] as const;
 const particles: Particle[] = [];
-
-class Particle {
-  constructor(
-    private x: number,
-    private y: number,
-    private radius: number,
-    private color: string | CanvasGradient | CanvasPattern,
-    private phase: 'grow' | 'shrink',
-  ) {}
-
-  draw() {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
-    c.closePath();
-  }
-
-  update() {
-    this.lifeCycle();
-    this.draw();
-  }
-
-  lifeCycle() {
-    if (this.phase === 'grow') {
-      this.radius += particleRadiusGrowSpeed;
-
-      if (this.radius > particleMaxRadius) {
-        this.phase = 'shrink';
-      }
-    }
-
-    if (this.phase === 'shrink') {
-      this.radius -= particleRadiusGrowSpeed;
-
-      if (this.radius < 0) {
-        this.radius = 0;
-        this.phase = 'grow';
-        this.randomPosition();
-      }
-    }
-  }
-
-  randomPosition() {
-    this.x = randomIntFromRange(0, innerWidth);
-    this.y = randomIntFromRange(0, innerHeight);
-  }
-}
 
 function createParticles() {
   for (let i = 0; i < particleCount; i++) {
@@ -100,18 +35,28 @@ function randomizeParticles() {
 }
 
 function logic({ currentFps }: { currentFps: number }) {
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  const context = getDrawingContext();
+  const mouse = getMousePosition();
+  const textPrinter = getTextPrinter(context);
+
+  clearDrawingArea();
 
   particles.forEach((object) => {
-    object.update();
+    object.update(context);
   });
 
-  c.fillStyle = 'black';
-  c.fillText(`FPS: ${currentFps}`, 0, 10);
+  textPrinter({ text: `FPS: ${currentFps}`, position: { x: 0, y: 10 } });
+  textPrinter({
+    text: 'HTML CANVAS BOILERPLATE',
+    color: randomRgba(),
+    position: { x: mouse.x, y: mouse.y },
+  });
 }
 
 createParticles();
 
-const fps = 24;
+registerClickHandler(randomizeParticles);
+
+const fps = 12;
 
 appLoop({ fps, onFrame: logic });
